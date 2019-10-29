db4657eef01bc3f81f807de34e2acc8ce90fed7b
nenew121 <33708352+nenew121@users.noreply.github.com>
2019-10-29 14:02:19 +0700

    Add files via upload

--- test.php
+++ test.php
@@ -1,6 +1,3 @@
-<?php include 'index.css';?>
-<?php include 'index.js';?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+	<link href="/index.css" media="screen" rel="stylesheet" type="text/css" />
 </head>
 <body>
     <span>Type "santa"</span>

a14b7f9f99f7fc4c6bd07c8d78c45ddc1212a622
nenew121 <33708352+nenew121@users.noreply.github.com>
2019-10-29 13:58:02 +0700

    Add files via upload

---
+++ test.php
@@ -0,0 +1,17 @@
+<?php include 'index.css';?>
+<?php include 'index.js';?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <span>Type "santa"</span>
+	<canvas id="canvas"></canvas>
+	<div class="santa hidden"></div>
+</body>
+</html>

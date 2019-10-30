3afe07340ac524f07529c877a9d6654625808825
nenew121 <33708352+nenew121@users.noreply.github.com>
2019-10-30 16:18:04 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-let dde = document.documentElement;
-dde.addEventListener("mousemove", e => {
-  let ow = dde.offsetWidth;
-  let oh = dde.offsetHeight;
-  dde.style.setProperty("--mouseX", e.clientX * 100 / ow + "%");
-  dde.style.setProperty("--mouseY", e.clientY * 100 / oh + "%");
-});
+//

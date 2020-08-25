1e69727bc9fd6484857fc14a5bff5e11d4515898
nenew121 <33708352+nenew121@users.noreply.github.com>
2020-08-26 00:44:16 +0700

    Update Controller.php

--- Controller.php
+++ Controller.php
@@ -1,8 +1,16 @@
 <?php
-function getImage() {
+function getImage2() {
   $image = 'http://images.itracki.com/2011/06/favicon.png';
   $imageData = base64_encode(file_get_contents($image));
   $src = 'data: '.mime_content_type($image).';base64,'.$imageData;
 return $src; 
+}
+function getImage() {
+  if(isset($_POST["date"]))
+  {
+    var date = $_POST["id"];
+    $data["name"] = "Hi ".date;
+  }
+  echo json_encode($data);
 }
 ?>

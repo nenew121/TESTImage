33361e52072f79c497b0731e643960420f312f51
nenew121 <33708352+nenew121@users.noreply.github.com>
2020-08-25 22:11:41 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,30 +13,14 @@ function Search() {
     success: function(data) {
       data.forEach(e => {
         if (e.date == $("#date").val()) {
-          $("tbody").append("<tr>
-            <td>1</td>
-            <td>897654654</td>
-            <td>John</td>
-            <td>Doe</td>
-            <td>john@example.com</td>
-            <td>0800000000</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>9980654155</td>
-            <td>Mary</td>
-            <td>Moe</td>
-            <td>mary@example.com</td>
-            <td>0800000000</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td>6540687987</td>
-            <td>July</td>
-            <td>Dooley</td>
-            <td>july@example.com</td>
-            <td>0800000000</td>
-          </tr>");
+          $("tbody").append("<tr>"+
+            "<td>1</td>"+
+            "<td>897654654</td>"+
+            "<td>John</td>"+
+            "<td>Doe</td>"+
+            "<td>john@example.com</td>"+
+            "<td>0800000000</td>"+
+          "</tr>");
           alert("Change Date " + $("#date").val());
         }
       });

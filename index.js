045d3ab55c5156eac8bb2fd8f3eb36bba9208eca
nenew121 <33708352+nenew121@users.noreply.github.com>
2020-08-25 12:20:43 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ $(document).ready(function(){
 });
 function getDate() {
 var txt = document.getElementById("date").value
-  alert("Change Date " + txt);
+  //alert("Change Date " + txt);
+  alert("Change Date " + $("#tt").val());
 }
 function input() {
 var txt = document.getElementById("tt").value

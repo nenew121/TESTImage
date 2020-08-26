9dfac7a1d1eb71fd025382f125e2744f28bb1939
nenew121 <33708352+nenew121@users.noreply.github.com>
2020-08-26 12:06:03 +0700

    Update controller.php

--- controller.php
+++ controller.php
@@ -5,6 +5,9 @@ if(isset($_POST["fnName"]))
     case 'getImage':
       getImage();
     break;
+    case 'getfile':
+      getfile();
+    break;
     default:
       $myObj->error = "Error switch case";
       echo json_encode($myObj);
@@ -26,6 +29,19 @@ function getImage() {
     $myObj->name = "John";
     $myObj->date = $date;
     echo json_encode($myObj);
+  }
+}
+
+function getfile() {
+  $directory = "https://shielded-mesa-66786.herokuapp.com/Image"; // dir location
+  if (glob($directory . "*.*") != false)
+  {
+   $filecount = count(glob($directory . "*.*"));
+   echo $filecount;
+  }
+  else
+  {
+   echo 0;
   }
 }
 ?>

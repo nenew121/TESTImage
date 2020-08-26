582bd8b130fdb67cd025639a587d3c3a06095fa0
nenew121 <33708352+nenew121@users.noreply.github.com>
2020-08-26 22:51:41 +0700

    Update controller.php

--- controller.php
+++ controller.php
@@ -13,40 +13,33 @@ if(isset($_POST["fnName"]))
 }
 
 function getImage() {
-  $url = "https://shielded-mesa-66786.herokuapp.com/Image/" . $_POST["date"] . ".png";
-  $file_name = basename($url);
-  $imageData = base64_encode(file_get_contents($url));
-  $src = 'data: '.mime_content_type($url).';base64,'.$imageData;
-  //$type = mime_content_type($url);
-  $types = glob("https://shielded-mesa-66786.herokuapp.com/Image/" . "*.*");
-  $type = "";
-  foreach($types as $dr)
-       {
-        $type = $dr;
-       }
-  
-  if($file_name) {
-    $myObj->name = strval($file_name);
-    $myObj->src = $src;
-    $myObj->type = $types;
-    $myObj->date1 = "date1";
-    echo json_encode($myObj);
-  } else {
-    $myObj->name = "Test";
-    $myObj->src = "src";
-    $myObj->type = "type";
-    $myObj->date1 = "date1";
-    echo json_encode($myObj);
-  }
-}
-
-function getImage2() {
   if(isset($_POST["date"]))
   {
-    $date = $_POST["date"];
-    $myObj->name = "John";
-    $myObj->date = $date;
-    echo json_encode($myObj);
+    $url = "https://shielded-mesa-66786.herokuapp.com/Image/" . $_POST["date"] . ".png";
+    $file_name = basename($url);
+    $imageData = base64_encode(file_get_contents($url));
+    $src = 'data: '.mime_content_type($url).';base64,'.$imageData;
+    //$type = mime_content_type($url);
+    $types = glob("https://shielded-mesa-66786.herokuapp.com/Image/" . "*.*");
+    $type = "";
+    foreach($types as $dr)
+    {
+      $type = $dr;
+    }
+
+    if($file_name) {
+      $myObj->name = strval($file_name);
+      $myObj->src = $src;
+      $myObj->type = $types;
+      $myObj->date1 = "date1";
+      echo json_encode($myObj);
+    } else {
+      $myObj->name = "Test";
+      $myObj->src = "src";
+      $myObj->type = "type";
+      $myObj->date1 = "date1";
+      echo json_encode($myObj);
+    }
   }
 }
 ?>

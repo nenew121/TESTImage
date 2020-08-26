4743e8f6ae534376b06220ac5c18e3f1ee35a501
nenew121 <33708352+nenew121@users.noreply.github.com>
2020-08-26 15:09:19 +0700

    Update controller.php

--- controller.php
+++ controller.php
@@ -5,9 +5,6 @@ if(isset($_POST["fnName"]))
     case 'getImage':
       getImage();
     break;
-    case 'getfile':
-      getfile();
-    break;
     default:
       $myObj->error = "Error switch case";
       echo json_encode($myObj);
@@ -15,29 +12,9 @@ if(isset($_POST["fnName"]))
   }
 }
 
-/*function getImage2() {
-  $image = 'http://images.itracki.com/2011/06/favicon.png';
-  $imageData = base64_encode(file_get_contents($image));
-  $src = 'data: '.mime_content_type($image).';base64,'.$imageData;
-return $src; 
-}*/
-
 function getImage() {
-  if(isset($_POST["date"]))
-  {
-    $date = $_POST["date"];
-    $myObj->name = "John";
-    $myObj->date = $date;
-    echo json_encode($myObj);
-  }
-}
-
-function getfile() {
   $url = "https://shielded-mesa-66786.herokuapp.com/Image/" . $_POST["date"] . ".png";
-  //$url = "https://shielded-mesa-66786.herokuapp.com/Image/2020-08-25.png";
   $file_name = basename($url);
-  //$file_data = file_get_contents($url);
-  
   $imageData = base64_encode(file_get_contents($url));
   $src = 'data: '.mime_content_type($url).';base64,'.$imageData;
   
@@ -50,6 +27,16 @@ function getfile() {
     $myObj->name = "Test";
     $myObj->src = "src";
     $myObj->date1 = "date1";
+    echo json_encode($myObj);
+  }
+}
+
+function getImage2() {
+  if(isset($_POST["date"]))
+  {
+    $date = $_POST["date"];
+    $myObj->name = "John";
+    $myObj->date = $date;
     echo json_encode($myObj);
   }
 }

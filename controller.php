83b8be7408e2faae477d7b04e42a3441b2409b22
nenew121 <33708352+nenew121@users.noreply.github.com>
2020-08-26 21:56:10 +0700

    Update controller.php

--- controller.php
+++ controller.php
@@ -18,12 +18,17 @@ function getImage() {
   $imageData = base64_encode(file_get_contents($url));
   $src = 'data: '.mime_content_type($url).';base64,'.$imageData;
   //$type = mime_content_type($url);
-  $type = glob("https://shielded-mesa-66786.herokuapp.com/Image/" . "*.*");
+  $types = glob("https://shielded-mesa-66786.herokuapp.com/Image/" . "*.*");
+  $type = "";
+  foreach($types as $dr)
+       {
+        $type += $dr;
+       }
   
   if($file_name) {
     $myObj->name = strval($file_name);
     $myObj->src = $src;
-    $myObj->type = $type[0];
+    $myObj->type = $type;
     $myObj->date1 = "date1";
     echo json_encode($myObj);
   } else {

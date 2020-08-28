a97fd1b1400761651845d41c3f70a1772792739b
nenew121 <33708352+nenew121@users.noreply.github.com>
2020-08-28 08:56:40 +0700

    Update controller.php

--- controller.php
+++ controller.php
@@ -12,7 +12,7 @@ if(isset($_POST["fnName"]))
   }
 }
 
-function getImage() {
+public static function getImage() {
   if(isset($_POST["date"]))
   {
     //$url = "https://shielded-mesa-66786.herokuapp.com/Image/" . $_POST["date"] . ".png";
@@ -41,5 +41,23 @@ function getImage() {
       }
     //}  
   }
+}
+
+public static function deleteDir($dirPath) {
+    if (! is_dir($dirPath)) {
+        throw new InvalidArgumentException("$dirPath must be a directory");
+    }
+    if (substr($dirPath, strlen($dirPath) - 1, 1) != '/') {
+        $dirPath .= '/';
+    }
+    $files = glob($dirPath . '*', GLOB_MARK);
+    foreach ($files as $file) {
+        if (is_dir($file)) {
+            self::deleteDir($file);
+        } else {
+            unlink($file);
+        }
+    }
+    rmdir($dirPath);
 }
 ?>

40850eb176e30bbb7575b178a89451476927796d
nenew121 <33708352+nenew121@users.noreply.github.com>
2020-08-31 22:36:40 +0700

    Update controller.php

--- controller.php
+++ controller.php
@@ -77,7 +77,7 @@ function testDelete() {
       }*/
     }
   }
-  $myObj->mass = "mass delete : " . $txt1 . " hi";
+  $myObj->mass = "mass delete : " . $txt1 . " hi" . $files[0];
   //$myObj->list = $arrName;
   $myObj->list1 = $arrTypeName;
   $myObj->list2 = $arrName;

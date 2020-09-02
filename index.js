9098da3b52a6c41086766a3c4ad67f599e61765d
nenew121 <33708352+nenew121@users.noreply.github.com>
2020-09-02 16:05:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
   Loading(true);
+  var slideIndex = 1;
+  showSlides(slideIndex);
   //alert("Ready");
   $("#search").click(function() {
     getImage();
@@ -79,3 +81,29 @@ function testdel() {
     Loading(false);
   }
 }
+
+//-------
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+//-------
